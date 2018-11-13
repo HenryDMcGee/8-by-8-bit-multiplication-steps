9057e4588e813e74fedee5ddc52b5e7ac9d709d2
HenryDMcGee <36491845+HenryDMcGee@users.noreply.github.com>
2018-11-13 00:57:17 -0500

    Add files via upload

---
+++ helperfunction.h
@@ -0,0 +1,14 @@
+//Henry McGee
+#ifndef HELPERFUNCTION_H
+#define HELPERFUNCTION_H
+#include <iostream>
+
+//adding two binary numbers
+//int* for passing an array
+void addbinary(int, int*, int*);
+//shifting product right
+void shiftRight(int*, int*);
+//converts binary to decimal
+int convertToDecimal(int*, int*);
+
+#endif
